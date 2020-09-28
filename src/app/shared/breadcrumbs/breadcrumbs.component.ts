41ab777bcdc6188b719658f06e5560d75fb25ad2
Wanderson Andrade <wandersonandrade.developer@gmail.com>
2020-09-28 11:30:21 -0300

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: []
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy{
 
-  constructor() { }
+  public titulo: string;
+  public tituloSubs$: Subscription;
 
-  ngOnInit() {
+  constructor( private router: Router ) {
+    this.tituloSubs$ = this.getArgumentosRuta()
+                        .subscribe( ({ titulo }) => {
+                            this.titulo = titulo;
+                            document.title = `AdminPro - ${ titulo }`;
+                          });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+   private getArgumentosRuta() {
+
+    return this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      filter( (event: ActivationEnd) => event.snapshot.firstChild === null ),
+      map( (event: ActivationEnd) => event.snapshot.data)
+    );
   }
 
 }
